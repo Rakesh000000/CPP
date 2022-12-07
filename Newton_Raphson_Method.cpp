15d0c47d1b7131adcba847989d1b528d50417eb3
Rakesh000000 <rp901522@gmail.com>
2022-12-07 22:44:57 +0545

    Numerical Method

--- Newton_Raphson_Method.cpp
+++ Newton_Raphson_Method.cpp
@@ -11,10 +11,11 @@ int main(){
     do{
     cout<<"Enter guess: ";cin>>x0;
     }while (g(x0)==0);
-
+    
     do{
-        x1 = x0 - (f(x0)/g(x0));
+        x1 = x0 - f(x0)/g(x0);
         err = fabs(x1-x0);
+        x0 = x1;
     }while(err>e);
     cout<<"The root is: "<<x0;
     return 0;
