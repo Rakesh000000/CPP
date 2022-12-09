1122114243cf9d410c5e343dbb827f1526f95c3b
Rakesh000000 <rp901522@gmail.com>
2022-12-09 18:12:35 +0545

    Numerical Method

---
+++ Gauss_jordan_Method.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    float mat[10][10], fact, pivot;
+    int n;
+
+    cout<<"Enter number of equations: "; cin>>n;
+    
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n+1;j++){
+            cout<<"A"<<i+1<<j+1<<":"; cin>>mat[i][j];
+        }
+    }
+    for(int k=0; k<n; k++){
+	        pivot = mat[k][k];
+	        
+			for(int j=0;j<n+1;j++){
+	            mat[k][j] = mat[k][j]/pivot;
+	        }
+        
+			for (int i=0; i<n; i++){
+				fact = mat[i][k];
+				
+				if(i != k){
+					for(int j=0; j<n+1; j++){
+	                    mat[i][j]= mat[i][j]-mat[k][j]*fact;
+	                }
+            	}
+        	}
+        	
+    }
+
+    for(int i=0; i<n; i++){
+        cout<<mat[i][n+1]<<endl;
+    }
+    
+    return 0;
+}
