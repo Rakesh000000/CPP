40cb843149390bece549a63a8357ea3df1d225bc
Rakesh000000 <rp901522@gmail.com>
2022-12-16 11:18:32 +0545

    Chapter_1

---
+++ Chapter_1(Language_Constructs)/0.Output.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std; // the input and output commands are included inside this namespace
+
+int main()
+{
+    cout<<"Hello";  // std::cout<<"hello" *If std namespace is not added*
+    return 0;
+}
--- output.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std; // the input and output commands are included inside this namespace
-
-int main()
-{
-    cout<<"Hello";  // std::cout<<"hello" *If std namespace is not added*
-    return 0;
-}
